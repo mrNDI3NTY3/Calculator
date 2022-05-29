e25b0eb48a404804837b7df65629a86d2ce30eb1
grishajeb123 <grishanya123@inbox.ru>
2022-05-29 19:12:36 +0500

    Update theory.py
    add 2 theory

--- theory.py
+++ theory.py
@@ -9,7 +9,10 @@ class phys_theory():
         print(Fore.CYAN + "При постоянных " + Fore.GREEN +  " температуре" +  Fore.CYAN + " и " + Fore.GREEN +  "массе газа" +  Fore.CYAN + " произведение " + Fore.GREEN +  " давления газа " +  Fore.CYAN + "на его " + Fore.GREEN +  "объём " +  Fore.CYAN + "постоянно.")
         print(Fore.CYAN + "\nВ математической форме это выглядит так: " + Fore.GREEN +  "pV=C" +  Fore.CYAN + "\n" + Fore.GREEN +  "p" +  Fore.CYAN + " — давление газа\n" + Fore.GREEN +  "V" +  Fore.CYAN + " — объём газа\n" + Fore.GREEN +  "C" +  Fore.CYAN + " — постоянная в оговоренных условиях величина")
         print(Fore.CYAN + "\nИз этого следует вот эта формула: "+ Fore.GREEN + "p = C/V")
-class geom_theory():
-    pass
-
+    def newton():
+        print(Fore.CYAN + "Первый закон: Всякое тело продолжает "+ Fore.GREEN + "удерживаться" +  Fore.CYAN + " в своём состоянии "+ Fore.GREEN + "покоя или равномерного и прямолинейного движения" +  Fore.CYAN + ", пока и поскольку оно не понуждается приложенными" +  Fore.GREEN + " силами" +  Fore.CYAN + " изменить это состояние.")
+        print(Fore.CYAN + "Второй закон: Изменение "+ Fore.GREEN + " количества движения" +  Fore.CYAN + " пропорционально приложенной "+ Fore.GREEN + "движущей силе" +  Fore.CYAN + " и происходит по направлению "+ Fore.GREEN + "той прямой" +  Fore.CYAN + ", по которой эта "+ Fore.GREEN + "сила" +  Fore.CYAN + " действует."
+        print(Fore.CYAN + "Третий закон: "+ Fore.GREEN + "Действию" +  Fore.CYAN + " всегда есть "+ Fore.GREEN + "равное и противоположное" +  Fore.CYAN + " противодействие, "+ Fore.GREEN + "иначе" +  Fore.CYAN + " — взаимодействия двух тел друг на друга между собою "+ Fore.GREEN + "равны" +  Fore.CYAN + " и направлены в противоположные стороны.")
+    def qulon():
+        print(Fore.CYAN + "Модуль силы взаимодействия "+ Fore.GREEN + "двух точечных зарядов" +  Fore.CYAN + " в вакууме "+ Fore.GREEN + "прямо пропорционален произведению модулей этих зарядов" +  Fore.CYAN + " и обратно пропорционален квадрату расстояния между ними.")
 phys_theory.boyl_mar()
